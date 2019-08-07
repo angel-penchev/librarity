50e13ae22bb86c06aa309868c5096b4175f108e5
Angel Penchev <angel.penchev.tsb@gmail.com>
2019-08-07 17:54:31 +0300

    Created the Angular Router
    Generated the components for most of the pages in the website and linked them all up in a RouterModule.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { BookComponent } from './components/book/book.component';
 import { BookListComponent } from './components/book-list/book-list.component';
@@ -14,6 +14,13 @@ import { SearchFormComponent } from './components/layout/search-form/search-form
 import { UserStatusComponent } from './components/layout/user-status/user-status.component';
 import { HomeBtnComponent } from './components/layout/home-btn/home-btn.component';
 import { BookSlideComponent } from './components/book-slide/book-slide.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { NotFoundComponent } from './components/pages/not-found/not-found.component';
+import { BookContentComponent } from './components/pages/book-content/book-content.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,12 +33,26 @@ import { BookSlideComponent } from './components/book-slide/book-slide.component
     UserStatusComponent,
     HomeBtnComponent,
     BookSlideComponent,
+    HomeComponent,
+    LoginComponent,
+    NotFoundComponent,
+    BookContentComponent,
+    AboutComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'books/:id', component: BookContentComponent },
+      { path: 'books', component: HomeComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: '**', component: NotFoundComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/components/pages/book-content/book-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-content',
+  templateUrl: './book-content.component.html',
+  styleUrls: ['./book-content.component.css']
+})
+export class BookContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
